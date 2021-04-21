ecf954b9e62c5ed3c710f60a9c75ade33d721823
darkfriend <maslov.tlt@gmail.com>
2021-04-21 21:12:07 +0300

    improved keysExists method

--- ArrayHelper.php
+++ ArrayHelper.php
@@ -6,7 +6,7 @@ namespace darkfriend\helpers;
  * Class ArrayHelper
  * @package darkfriend\devhelpers
  * @author darkfriend <hi@darkfriend.ru>
- * @version 1.0.4
+ * @version 1.0.5
  */
 class ArrayHelper
 {
@@ -89,12 +89,16 @@ class ArrayHelper
      * Check includes keys in $sourceArray
      * @param array $arKeys
      * @param array $sourceArray
+     * @param bool $strict all $arKeys in $sourceArray
      * @return bool
      * @since 1.0.2
      */
-    static public function keysExists($arKeys, $sourceArray)
+    public static function keysExists(array $arKeys, array $sourceArray, $strict=false)
     {
-        return !array_diff_key(array_flip($arKeys), $sourceArray);
+        if($strict) {
+            return !\array_diff_key(\array_flip($arKeys), $sourceArray);
+        }
+        return $arKeys !== \array_diff($arKeys, \array_flip($sourceArray));
     }
 
     /**
